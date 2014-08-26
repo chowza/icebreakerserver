cd72e5c13e38ac55f0578704011986511ebc3b6b
Terence Chow <terence.chow.1986@gmail.com>
2014-08-26 14:43:17 -0400

    add remote url downloading with paperclip

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
 
 	has_many :matches
 	has_many :messages, through: :matches
-	
+
 	has_attached_file :picture1, styles: {
 		thumb: '100x100>',
     	square: '200x200#',
@@ -37,4 +37,31 @@ class Profile < ActiveRecord::Base
     validates_attachment_content_type :picture3, :content_type => /\Aimage\/.*\Z/
     validates_attachment_content_type :picture4, :content_type => /\Aimage\/.*\Z/
     validates_attachment_content_type :picture5, :content_type => /\Aimage\/.*\Z/
+    validates_with AttachmentSizeValidator, :attributes => :picture1, :less_than => 1.megabytes
+    validates_with AttachmentSizeValidator, :attributes => :picture2, :less_than => 1.megabytes
+    validates_with AttachmentSizeValidator, :attributes => :picture3, :less_than => 1.megabytes
+    validates_with AttachmentSizeValidator, :attributes => :picture4, :less_than => 1.megabytes
+    validates_with AttachmentSizeValidator, :attributes => :picture5, :less_than => 1.megabytes
+
+
+    def picture1_from_url(url)
+	    self.picture1 = URI.parse(url) unless url.blank?
+  	end
+
+  	def picture2_from_url(url)
+	    self.picture2 = URI.parse(url) unless url.blank?
+  	end
+
+  	def picture3_from_url(url)
+	    self.picture3 = URI.parse(url) unless url.blank?
+  	end
+
+  	def picture4_from_url(url)
+	    self.picture4 = URI.parse(url) unless url.blank?
+  	end
+
+  	def picture5_from_url(url)
+	    self.picture5 = URI.parse(url) unless url.blank?
+  	end
+
 end
