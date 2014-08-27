cd83238676107f49b231bbea5a3ecf8c14d32ab0
Terence Chow <terence.chow.1986@gmail.com>
2014-08-26 20:58:03 -0400

    anything

--- db/migrate/20140826185157_add_picture_remote_url_to_profiles.rb
+++
@@ -1,9 +0,0 @@
-class AddPictureRemoteUrlToProfiles < ActiveRecord::Migration
-  def change
-    add_column :profiles, :picture1_from_url, :string
-    add_column :profiles, :picture2_from_url, :string
-    add_column :profiles, :picture3_from_url, :string
-    add_column :profiles, :picture4_from_url, :string
-    add_column :profiles, :picture5_from_url, :string
-  end
-end
