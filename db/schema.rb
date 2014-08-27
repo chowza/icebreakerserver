85c9bb1d73c8136ba625e58d840b1f2c3148212f
Terence Chow <terence.chow.1986@gmail.com>
2014-08-26 20:51:54 -0400

    random changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827000600) do
+ActiveRecord::Schema.define(version: 20140827004939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20140827000600) do
     t.integer  "picture5_file_size"
     t.datetime "picture5_updated_at"
     t.string   "preferred_gender"
+    t.string   "gender"
   end
 
 end
