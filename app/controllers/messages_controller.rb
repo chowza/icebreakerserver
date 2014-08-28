9b88c31155bf514c1fbf4c4b67c7f593665222a6
Terence Chow <terence.chow.1986@gmail.com>
2014-08-28 16:12:14 -0400

    testing gcm

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,4 +7,26 @@ class MessagesController < ApplicationController
 		@messages = Message.where("recipient_id = ? AND profile_id = ?", [@profile['id'],params[:recipient_id]],[@profile['id'],params[:recipient_id]]).order(:created_at)
 		render json: @messages
 	end
+
+	def create
+		@message = Message.new(message_params)
+		
+		if @message.save
+
+            gcm = ::GCM.new(ENV['GCM_API_KEY'])
+#            @recipient = Profile.find(params[:recipient_id])
+			@recipient = Profile.find_by_facebook_id(10100675664421760) # this used for testing purposes
+            gcm.send_notification({registration_ids:[@recipient['client_identification_sequence']],data:{message:params[:content],msgcnt:"1",sender_name:params[:sender_name],facebook_id:params[:facebook_id]}})
+
+			render json: @profile
+		else
+			render 'failed to save'
+		end
+
+	end
+
+	def message_params
+      params.require(:message).permit(:content, :profile_id, :recipient_id,:facebook_id,:sender_name) #facebook_id = sender facebook id
+  	end
+
 end
