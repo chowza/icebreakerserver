986feb81875c5092b29e68bb12768cae0653e143
Terence Chow <terence.chow.1986@gmail.com>
2014-08-28 06:21:11 -0400

    added gcm module in lib, changed config to recognize gcm, added a route and updated matches and messages controllers to recognize gcm

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,11 @@
 class MatchesController < ApplicationController
-	before_filter :cors_preflight_check
+    require 'gcm'
+    # gcm = ::GCM.new(api_key)
+    # gcm.send_notification({registration_ids: ["4sdsx", "8sdsd"], data: {score: "5x1"}})
+    
+	  before_filter :cors_preflight_check
   	after_filter :cors_set_access_control_headers
+
 
   	def show
   		#GET call to matches/:id - used to show an individual's matches
@@ -10,7 +15,21 @@ class MatchesController < ApplicationController
 
   	def create
   		#POST call to matches - used to add a new like or dislike
-  		
+      @match = Match.new(match_params)
+      if @match.save
+        if params[:likes]
+          # TODO
+          #since you liked, check if you were liked back and send a notification and save that a match was made
+        else
+          #since you didn't like, continue 
+        end
+      else
+        #error saving...investigate what the error is
+      end
   	end
+
+  def match_params
+      params.require(:match).permit(:swipee_id, :likes, :match,:swipee_name,:profile_id)
+  end
 
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
-	
-
+	require 'gcm'
+  	# gcm = ::GCM.new(api_key)
+  	# gcm.send_notification({registration_ids: ["4sdsx", "8sdsd"], data: {score: "5x1"}})
 	def show
 		@profile = Profile.find_by_facebook_id(params[:id])
 		@messages = Message.where("recipient_id = ? AND profile_id = ?", [@profile['id'],params[:recipient_id]],[@profile['id'],params[:recipient_id]]).order(:created_at)
