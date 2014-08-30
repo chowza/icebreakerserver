6573fff4c17abc64bab34ec683991055497b0744
Terence Chow <terence.chow.1986@gmail.com>
2014-08-29 23:40:54 -0400

    r

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -18,7 +18,7 @@ class MatchesController < ApplicationController
           # TODO
           #since you liked, check if you were liked back and send a notification and save that a match was made
           @recipient = Match.where("profile_id = ? and swipee_id = ?",params[:match][:swipee_id],params[:match][:profile_id])[0]
-          if @recipient.exists?
+          if !@recipient.nil?
             if @recipient['likes']
               # 2 likes, send match messages and then save that match = true
 
