3bcc4c4f5831cb4b66227b6eab1d74fb8666e347
Terence Chow <terence.chow.1986@gmail.com>
2014-09-03 18:07:32 -0400

    .

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -50,7 +50,7 @@ class MatchesController < ApplicationController
 
               #notification to recipient
               if @recipient.profile.push_type == 'gcm'
-                gcm.send([@match.profile.client_identification_sequence],data:{message:"Meet or Chat-for-24 with "+@match.profile.first_name+"!",title:"You have a new match",notId:"1",swipee_id:@match.profile.id,swipee_name:@match.profile.first_name,recipient_facebook_id:@match.profile.facebook_id})
+                gcm.send([@recipient.profile.client_identification_sequence],data:{message:"Meet or Chat-for-24 with "+@match.profile.first_name+"!",title:"You have a new match",notId:"1",swipee_id:@match.profile.id,swipee_name:@match.profile.first_name,recipient_facebook_id:@match.profile.facebook_id})
               elsif @recipient.profile.push_type == 'apns'
                 #TODO send apple device
               elsif @recipient.profile.push_type == 'mpns'
