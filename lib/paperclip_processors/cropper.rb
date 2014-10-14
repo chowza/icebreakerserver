d734c19bbac09f2b2cceebd5918ce5dfdd9b4112
Terence Chow <terence.chow.1986@gmail.com>
2014-10-14 17:13:08 -0400

    t

--- lib/paperclip_processors/cropper.rb
+++ lib/paperclip_processors/cropper.rb
@@ -1,21 +1,14 @@
 module Paperclip
   class Cropper < Thumbnail
-    def transformation_command
-      if crop_command
-        # super returns an array like this: ["-resize", "100x", "-crop", "100x100+0+0", "+repage"]
-        crop_command + super.join(' ').sub(/ -crop \S+/, '').split(' ') 
-      else
-        super
-      end
+    def initialize(file, options = {}, attachment = nil)
+      super
+      @current_geometry.width = target.crop_width
+      @current_geometry.height = target.crop_height
     end
-    
-    def crop_command
+    def transformation_command
       target = @attachment.instance
-      puts "testingggggggggggggggg"
-      puts "testing workeddddddddddddddddd" + String.try_convert(target.crop_w)
-      if target.cropping?
-         ["-crop", "#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}"]
-      end
+      crop_command = ["-crop", "#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}"]
+      crop_command + super
     end
   end
 end
