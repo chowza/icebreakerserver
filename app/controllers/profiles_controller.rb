3c9d1ff48d7bcd168695b1aa358b32150627c9f1
unknown <terence.chow.1986@gmail.com>
2014-10-30 00:36:06 -0400

    permit array

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -65,10 +65,13 @@ class ProfilesController < ApplicationController
 
 		@profile = Profile.find_by_facebook_id(params[:id])
 
-		puts "----------------"
-		puts params[:profile]
+		# set order array to be blank if it does not exist
+		params[:profile][:order] ||= []
+
+		puts "-------------"
 		puts profile_params
-		puts "----------------"
+		puts "-------------"
+		
 		# percent messaged can also be updated with a rake task every day...this would make this section faster.
 
 		#update percent messaged
@@ -123,7 +126,7 @@ class ProfilesController < ApplicationController
 	private
 
 	def profile_params
-	    params.require(:profile).permit(:facebook_id, :age, :first_name, :latitude, :longitude, :answer1, :answer2, :feet, :inches, :blurb, :preferred_min_age,:preferred_max_age, :preferred_gender, :preferred_sound, :preferred_distance, :gender, :order, :picture1, :picture2, :picture3, :picture4, :picture5,:client_identification_sequence,:push_type,:percent_messaged,:crop_w,:crop_x,:crop_h,:crop_y,:looks_last_5_average_rating,:answer1_last_5_average_rating,:answer2_last_5_average_rating,:answer3_last_5_average_rating) #:timezone,:remember_availability, :today_before_five,:today_after_five,:tomorrow_before_five,:tomorrow_after_five,:updated_availability,
+	    params.require(:profile).permit(:facebook_id, :age, :first_name, :latitude, :longitude, :answer1, :answer2, :feet, :inches, :blurb, :preferred_min_age,:preferred_max_age, :preferred_gender, :preferred_sound, :preferred_distance, :gender, :picture1, :picture2, :picture3, :picture4, :picture5,:client_identification_sequence,:push_type,:percent_messaged,:crop_w,:crop_x,:crop_h,:crop_y,:looks_last_5_average_rating,:answer1_last_5_average_rating,:answer2_last_5_average_rating,:answer3_last_5_average_rating,order:[]) #:timezone,:remember_availability, :today_before_five,:today_after_five,:tomorrow_before_five,:tomorrow_after_five,:updated_availability,
 	end
 
 	def picture1_url?
